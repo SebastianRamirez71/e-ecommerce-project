072a549e0e6a9fbd700d3d33369a29d5a691a8d3
SebastianRamirez71 <sebaramirezx2010@gmail.com>
2023-11-30 19:02:12 -0300

    added carousel in Catalog

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { Card, Tag } from "antd";
 import Meta from "antd/es/card/Meta";
-import { Carousel } from 'flowbite-react';
-
-function CarouselP({products}) {
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+  
+function CarouselP({ products }) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  };
   return (
-    <div className="grid h-56 grid-cols-2 gap-4 sm:h-64 xl:h-80 2xl:h-96">
-      {/* Aquí puedes usar el array de productos (products) */}
-      {products.map((product) => (
-        <Carousel>
-        <Card
-              style={{ marginBottom: "20px" }}
-              className="cardProduct"
+    <div className="w-3/4 m-auto">
+      <div className="mt-20">
+        <Slider {...settings} >
+          {products.map((product) => (
+            <Card 
+              className="bg-white h-[450px] text-black rounded-xl"
               cover={
                 <img
                   style={{ width: "100%" }}
@@ -25,8 +33,9 @@ function CarouselP({products}) {
               <p>${product.price}</p>
               {product.stock > 0 ? <Tag>SIN STOCK</Tag> : null}
             </Card>
-        </Carousel>
-      ))}
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 }
